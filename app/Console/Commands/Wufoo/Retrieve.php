66910ecdc6437196e6cbe595e2c14daedbfa189b
Akeem Palmer <42576837+DevOps242@users.noreply.github.com>
2023-02-09 23:00:30 -0500

    Update: added the functionality to retrieve fields and confirm name and hash is not empty

--- app/Console/Commands/Wufoo/Retrieve.php
+++ app/Console/Commands/Wufoo/Retrieve.php
@@ -23,7 +23,11 @@ class Retrieve extends Command
 
     protected $description = 'Gets all the form entries for each form.';
 
-    // protected $formsRetrieved = 0;
+    protected $formsRetrieved = 0;
+
+    protected $errorForms = [];
+
+    protected $errorFormsCount = 0;
 
 
     /**
@@ -31,7 +35,7 @@ class Retrieve extends Command
      */
     public function handle()
     {
-        $this->info("Wufoo Retrieve  Process - Starting process......");
+        $this->info("Wufoo Retrieve Process - Starting process......");
 
         // Get the configuration files from the .env
         $APIKEY = \env('APIKEY');
@@ -49,9 +53,50 @@ class Retrieve extends Command
             die("There was an error in getting the forms");
         }
 
+        // Create the empty array to hold the forms name and hash value to pull each entry.
+        $formsHash = [];
+
+        // Get all forms name and hash value
+        foreach ($availableForms['Forms'] as $forms) {
+            array_push($formsHash, [
+                "formName" => $forms["Name"],
+                "formHash" => $forms["Hash"],
+                "formFields" => $wufoo->getFormFields($forms["Hash"]),  // Get all of the form fields.
+                "statusReport" => $this->checkFormStatus($forms["Name"], $forms["Hash"])
+            ]);
+        };
+
+        foreach ($formsHash as $forms) {
+            if ($forms["statusReport"] == "Error") {
+                array_push($errorForms, $forms);
+                Log::critical("Form hash with error => \nName:" . $forms['Name'] . "\nHash: " . $forms['Hash'] . "\nStatus: " . $forms["statusReport"]);
+            }
+        }
+
+
+        // retrieve form fields.
+        $formsRetrieved = count($formsHash);
+
         // Let the user know forms were pulled successfully.
-        $this->info("\n Wufoo Retrieve  Process - Forms collected successfully......");
+        $this->info("\nWufoo Retrieve Process - " . $formsRetrieved . " Forms collected successfully......");
 
         // Each form has their records.
+
+        dd($formsHash);
+
+
+        // for each form an XLSX is generated with header line and data and stored in a local directory
+        // - xlsx is named after form name
+    }
+
+
+
+    private function checkFormStatus($name, $hash)
+    {
+        if (empty($name) && empty($hash)) {
+            return "Error";
+        }
+
+        return "Pass";
     }
 }
--- src/Services/Wufoo.php
+++ src/Services/Wufoo.php
@@ -84,4 +84,46 @@ final class Wufoo
             Log::critical("There was an error pulling the entries for this form");
         }
     }
+
+    public function getFormFields($hash)
+    {
+
+        try {
+            $curl = curl_init("https://$this->subDomain.wufoo.com/api/v3/forms/$hash/fields.json");
+
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_USERPWD, $this->apiKey . ":" . $this->password);
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($curl, CURLOPT_USERAGENT, 'Wufoo Sample Code');
+
+            $response = curl_exec($curl);
+            $resultStatus = curl_getinfo($curl);
+
+            curl_close($curl);
+
+            if ($resultStatus['http_code'] != 200) {
+                echo 'Call Failed ' . print_r($resultStatus);
+            }
+
+            // get the response as an array
+            $jsonResponse = json_decode($response, true);
+
+            $formFields = [];
+
+            // Get only the title of the fields.
+            foreach ($jsonResponse["Fields"] as $formField) {
+                array_push($formFields, $formField["Title"]);
+            }
+
+            if (!isset($formFields)) {
+                Log::critical("Could not attrieve form field");
+            }
+
+            return $formFields;
+        } catch (Exception $error) {
+            Log::critical("There was an error pulling the entries for this form " . $error->getMessage());
+        }
+    }
 }
